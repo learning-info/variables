9318c23c6956a029e3b10feb2df2aba9024495b9
Jose Alfredo Chavarria <46409841+Jacm1607@users.noreply.github.com>
2023-05-26 22:05:20 -0400

    Add files via upload

--- test.js
+++ test.js
@@ -1,41 +1,26 @@
-/*
-1. CREAR 10 VARIABLES Y ASIGNARLE EL VALOR QUE HAGAN REFERENCIA A UNA PERSONA
+<!DOCTYPE html>
+<html>
 
-EJEMPLO 
-let primerNombre = "JOSE"
-let edad = 18
-*/
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>replit</title>
+  <link href="style.css" rel="stylesheet" type="text/css" />
+</head>
 
-let nombre = "JOSE"
-let apellido = "CHAVARRIA"
-let edad = 24;
-let profesion = "ING SISTEMA"
-let telefono = 69113464;
-let especialidad = false;
-let diplomado = true;
-let lugarTrabajo = "BANCO GANADERO";
-let cargoLaboral = "DESARROLLADOR SOFTWARE"
-let docente = true;
+<body>
+  Hello world
+  <!-- <script src="ejercicio3.js"></script> -->
+  <script src="funciones/practico_funciones.js"></script>
+  <!-- <script src="practico_aleVillagomez.js"></script> -->
 
-/*
-2. CREAR UN ARRAY Y ASIGNAR 6 DE LOS VALORES DE LA PERSONA
-tips: let persona = ["Jose",...];
-*/
-let jose = ["CHAVARRIA", 24, 69113464, "ING SISTEMA", "BANCO GANADERO", "DESARROLLADOR SOFTWARE"];
 
-/*3. REALIZAR LA VALIDACION SI LA PERSONA ES MAYOR DE EDAD "18".
-if(<condicion>) {
-    console.log(<mensaje>)
-} else {
-    console.log(<mensaje>)
-}
-- SI ES VERDADERO MOSTRAR EN CONSOLA EL MENSAJE "LA PERSONA ES MAYOR DE EDAD"
+  <!--
+  This script places a badge on your repl's full-browser view back to your repl's cover
+  page. Try various colors for the theme: dark, light, red, orange, yellow, lime, green,
+  teal, blue, blurple, magenta, pink!
+  -->
+  <script src="https://replit.com/public/js/replit-uahsebwwbadge-v2.js" theme="dark" position="bottom-right"></script>
+</body>
 
-- SI ES FALSO MOSTRAR EN CONSOLA EL MENSAJE "LA PERSONA ES MENOR DE EDAD"
-
-*/
-if (edad >= 18) {
-    console.log("LA PERSONA ES MAYOR DE EDAD");
-} else {
-    console.log("LA PERSONA ES MENOR DE EDAD");
-}
+</html>
